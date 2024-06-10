607fa12f9c547175ba67b30f647e41e085519251
chetan sharma <cs7804@gmail.com>
2024-06-10 22:37:09 +0530

    added stars particles

--- src/App.jsx
+++ src/App.jsx
@@ -18,31 +18,71 @@ function App() {
 
 
       <div className=" w-[1250px] left-[50%] translate-x-[-50%]  h-full z-[-10] absolute  top-[-280px]  flex flex-row   items-start justify-center ">
-        <video className="w-full h-auto  rotate-180 "  playsInline={true} loop={true} autoPlay={true} src="/orbit.webm"></video>
+        <video className="w-full h-auto  rotate-180 " playsInline={true} loop muted autoPlay src="/orbit.webm"></video>
       </div>
-      <nav className='Navbar justify-around items-center flex text-white h-[68px]   w-full'>
-        <div className='flex flex-col'>
-          <a href='/' className='font-bold text-lg'>Chetan</a>
-          <a href='/' className='text-xs'>Frontend Developer </a>
+      <nav className='Navbar  fixed top-0 justify-around items-center z-20 flex text-white h-[68px]   w-full'>
+        <div className='flex flex-col '>
+          <a href='/' className='font-bold text-lg font-poppins'>Chetan</a>
+          <a href='/' className='text-xs font-poppins'>Frontend Developer </a>
         </div>
-        <div className='flex text-lg gap-10'>
-          <a href='#'>About</a>
-          <a href='#'>Work</a>
-          <a href='Contacts'>Contact</a>
+        <div className='flex text-lg gap-10 '>
+          <a href='#' className='font-poppins'>About</a>
+          <a href='#' className='font-poppins'>Work</a>
+          <a href='Contacts' className='font-poppins'>Contact</a>
         </div>
       </nav>
-      <div className='flex md:w-[70vw] gap-2 items-start mx-auto mt-36'>
-        <div className="flex flex-col  justify-center items-center mt-9">
-          <div className="w-5 h-5 rounded-full bg-purple-500 to-cyan-500"></div>
-          <div className="w-1 relative z-10  sm:h-[450px] h-[200px] violet-gradient sm:mt-0"></div>
+      <section>
+        <div className='flex lg:w-[70vw] md:w-[90vw] h-[450px]  justify-between items-start mx-auto mt-44'>
+          <div className='flex w-[50%]'>
+            <div className="flex flex-col  justify-center items-center mt-9">
+              <div className="w-5 h-5 rounded-full bg-purple-500 to-cyan-500"></div>
+              <div className="w-1 relative z-10  sm:h-[450px] h-[200px] violet-gradient sm:mt-0"></div>
+            </div>
+            <div className='flex flex-col p-1 gap-6 mt-14 md:w-[400px] text-white'>
+              <h1 className='flex  font-extrabold  gap-y-6 flex-wrap text-7xl whitespace-pre-line '>Hi, I'm&nbsp; <p className='name-gradient font-poppins'> Chetan</p></h1>
+              <span className='text-2xl font-poppins'>
+                A passionate Front-End Developer based in Bangalore, India.
+              </span>
+            </div>
+          </div>
+          <div className='w-[50%] h-[450px]'>
+            <img src='/skill-svg.svg' className='w-full h-full object-contain '></img>
+          </div>
+
         </div>
-        <div className='flex flex-col p-1 gap-6 mt-14 md:w-[400px] text-white'>
-          <h1 className='flex  font-extrabold  gap-y-6 flex-wrap text-8xl whitespace-pre-line '>Hi, I'm&nbsp; <p className='name-gradient font-poppins'> Chetan</p></h1>
-          <span className='text-2xl font-poppins'>
-          A passionate Front-End Developer based in Bangalore, India.
-          </span>
-        </div>
+      </section>
+      <div className='w-screen flex items-center justify-center '>
+        <a href='#work'>
+          <div className='w-[35px] relative flex items-center justify-center h-[60px] border-4 rounded-3xl '>
+            <div className="vert-move w-3 h-3  bg-white rounded-full" >
+
+            </div>
+          </div>
+        </a>
       </div>
+      <section id='work'>
+        <div className='h-[calc(100vh-68px)] w-screen flex items-center justify-center '>
+          <div className='flex flex-col gap-3 w-[76%] mx-auto  text-white'>
+            <div className='flex flex-col gap-1'>
+              <span className='text-xl text-[#A4A0BD]'>WELCOME TO THE JOURNEY</span>
+              <span className='text-4xl font-bold font-poppins text-blue-500'>Explore the Overview</span>
+            </div>
+            <span className=' w-[50%] font-poppins text-[#A4A0BD] font-extralight'>Hey there! 👋 I'm a passionate software developer, skilled in JavaScript and TypeScript. With expertise in Angular, Dotnet , and Three.js, I create beautiful and efficient solutions. Let's collaborate to transform your ideas into user-friendly, scalable realities. Ready for the journey? 🚀</span>
+            <div className='w-full flex h-[400px] relative justify-evenly z-20 items-center '>
+            <div className='w-[250px] h-[280px] bg-slate-600 rounded-2xl '>  
+            </div>
+            <div className='w-[250px] h-[280px] bg-slate-600 rounded-2xl '>  
+            </div>
+            <div className='w-[250px] h-[280px] bg-slate-600 rounded-2xl '>  
+            </div>
+            <div className='w-[250px] h-[280px] bg-slate-600 rounded-2xl '>  
+            </div>
+           
+          </div>
+          </div>
+         
+        </div>
+      </section>
     </>
   )
 }
